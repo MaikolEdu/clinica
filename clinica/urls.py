4ea22ca7cb39ed6a9f74f46b63b3c8144f61834a
michael0608 <micky_1390@outlook.com>
2013-01-09 18:42:42 -0500

    segunda fase

--- clinica/urls.py
+++ clinica/urls.py
@@ -4,6 +4,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^lista-paciente/$','principal.views.lista_pacientes'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- principal/views.py
+++ principal/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from principal.models import Paciente,Cita,Pago
+from django.shortcuts import render_to_response
+
+def lista_pacientes(request):
+	paciente = Paciente.objects.all()
+	return render_to_response('lista_pacientes.html',{'paciente':paciente})
